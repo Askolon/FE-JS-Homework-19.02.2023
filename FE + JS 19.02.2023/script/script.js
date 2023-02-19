9e276d1ea5e76bf594e3e1cb1e08a0971b6b6bfc
Askolon <39673233+Askolon@users.noreply.github.com>
2023-02-19 10:00:06 +0100

    Add files via upload

---
+++ FE + JS 19.02.2023/script/script.js
@@ -0,0 +1,30 @@
+const red = document.querySelector('.red');
+
+red.addEventListener('click', function(){
+    alert("Hi, its RED Color! #ff0000")
+});
+
+const blue = document.querySelector('.blue');
+
+blue.addEventListener('click', function(){
+    alert("Hi, its Blue Color! #016bc7")
+});
+
+const yellow = document.querySelector('.yellow');
+
+yellow.addEventListener('click', function(){
+    alert("Hi, its YELLOW Color! #fafe02")
+});
+
+const green = document.querySelector('.green');
+
+green.addEventListener('click', function(){
+    alert("Hi, its GREEN Color! #08ae08")
+});
+
+const black = document.querySelector('.black');
+
+black.addEventListener('click', function(){
+    alert("Hi, its BLACK Color! #000000")
+});
+    
